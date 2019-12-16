9c04b0d427c61aa47a6b8ecf1eaa2bd89579ee95
Derrick Lau <derrcikalau@gmail.com>
2019-12-16 13:49:25 +0000

    adding New function to AWS lamda

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -32,3 +32,24 @@ const handleRegister = (req, res, db, bcrypt) => {
 module.exports = {
   handleRegister: handleRegister
 };
+
+// DEMO (WITHOUT KNEX)
+// async () => {
+//   // note: we don't try/catch this because if connecting throws an exception
+//   // we don't need to dispose of the client (it will be undefined)
+//   const client = await pool.connect()
+//   try {
+//     await client.query('BEGIN')
+//     const queryText = 'INSERT INTO users(name) VALUES($1) RETURNING id'
+//     const res = await client.query(queryText, ['brianc'])
+//     const insertPhotoText = 'INSERT INTO photos(user_id, photo_url) VALUES ($1, $2)'
+//     const insertPhotoValues = [res.rows[0].id, 's3.bucket.foo']
+//     await client.query(insertPhotoText, insertPhotoValues)
+//     await client.query('COMMIT')
+//   } catch (e) {
+//     await client.query('ROLLBACK')
+//     throw e
+//   } finally {
+//     client.release() // MULTIPLE REQS DONT END()
+//   }
+// })().catch(e => console.error(e.stack))
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { render } from 'react-dom';
 
-const Rank = ({name, entries}) => {
-	return(
-		<div className='white f5'>
-			<div className='center'>
-				{`${name}, your current entry count is...`}
+class Rank extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			emoji:''
+		}
+	}
+
+	componentDidMount(){
+		this.generateEmoji(this.props.entries)
+	}
+
+	generateEmoji = (entries) => {
+		fetch(`https://nsdwl1q8kg.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`)
+			.then (res => res.json())
+			.then(data => this.setState({ emoji: data.input}))
+			.catch(console.log)
+	}
+	render() {
+		return(
+			<div className='white f5'>
+				<div className='center'>
+					{`${this.props.name}, your current entry count is...`}
+				</div>
+				<div className='center f3' >
+					{this.props.entries}
+				</div>
+				<div className='center f3' >
+					{`Rank Badge: ${this.state.emoji}`}
+				</div>
 			</div>
-			<div className='center f3' >
-				{entries}
-			</div>
-		</div>
-	);
+		);
+	}
+
 }
 
 export default Rank;
